02561b524bc4a738a059b8e5a5d75934fc10d20d
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-27 19:49:46 +0300

    fix IP address ranges for subnets

--- networks.tf
+++ networks.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "subnetbe01" {
   name                 = "Subnetbe01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet0.name
-  address_prefixes     = ["10.0.10.0/24"]
+  address_prefixes     = ["10.0.20.0/24"]
 }
 
 # Create subnetfe01
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "subnetfe01" {
   name                 = "Subnetfe01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet0.name
-  address_prefixes     = ["10.0.20.0/24"]
+  address_prefixes     = ["10.0.10.0/24"]
 }
 
 # Create public IP for VMLUE01

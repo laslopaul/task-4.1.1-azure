4507eef3a2d6ef6d18031b3c11bc9f02a75541ac
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-27 19:42:08 +0300

    init: start project

---
+++ networks.tf
@@ -0,0 +1,37 @@
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "RG_TASK_411"
+  location = "eastus"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet0" {
+  name                = "VirtualNet411"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnetbe01
+resource "azurerm_subnet" "subnetbe01" {
+  name                 = "Subnetbe01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.0.10.0/24"]
+}
+
+# Create subnetfe01
+resource "azurerm_subnet" "subnetfe01" {
+  name                 = "Subnetfe01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.0.20.0/24"]
+}
+
+# Create public IP for VMLUE01
+resource "azurerm_public_ip" "publicip_vmlue01" {
+  name                = "PublicIP_VMLUE01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
---
+++ sec_groups.tf
@@ -0,0 +1,68 @@
+# Create security group for VMLUE01, making it accessible from the Internet
+resource "azurerm_network_security_group" "nsg_vmlue01" {
+  name                = "NSG_VMLUE01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSHInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowFTPInbound"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "21"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create security group for subnetbe01
+resource "azurerm_network_security_group" "nsg_backend" {
+  name                = "NSG_Backend"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowVMLUEInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.20.2/32"
+    destination_address_prefix = "10.0.20.0/24"
+  }
+
+  security_rule {
+    name                       = "DenyVnetInbound"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "10.0.20.0/24"
+  }
+
+}
+
+# Connect the security group to subnetbe01
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc_subnetbe01" {
+  subnet_id                 = azurerm_subnet.subnetbe01.id
+  network_security_group_id = azurerm_network_security_group.nsg_backend.id
+}

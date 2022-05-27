9764ccfb8b0ce7bcde857a2e0da59e1ff6c68c19
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-27 21:48:32 +0300

    update security rule name

--- sec_groups.tf
+++ sec_groups.tf
@@ -36,7 +36,7 @@ resource "azurerm_network_security_group" "nsg_backend" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "AllowVMLUEInbound"
+    name                       = "AllowInboundFromVMLUE"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"

5c54475cbab16c00551adbbffb600ee5ae333dd0
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-30 00:22:11 +0300

    refactor VM files

--- vmlu.tf
+++ vmlu.tf
@@ -1,8 +1,9 @@
-# ----VMLU01 CONFIGURATION FILE----
+# ----VMLU01 & VMLU02 CONFIGURATION FILE----
 
-# Create network interface
-resource "azurerm_network_interface" "nic_vmlu01" {
-  name                = "NIC_VMLU01"
+# Create network interfaces
+resource "azurerm_network_interface" "nic_vmlu" {
+  count               = 2
+  name                = "NIC_VMLU0${count.index + 1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -10,22 +11,24 @@ resource "azurerm_network_interface" "nic_vmlu01" {
     name                          = "internal_static"
     subnet_id                     = azurerm_subnet.subnetbe01.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.20.11"
+    private_ip_address            = "10.0.20.1${count.index + 1}"
   }
 }
 
-# Create (and display) an SSH key
-resource "tls_private_key" "ssh_vmlu01" {
+# Create SSH keys
+resource "tls_private_key" "ssh_vmlu" {
+  count     = 2
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "vmlu01" {
-  name                  = "VMLU01"
+# Create virtual machines
+resource "azurerm_linux_virtual_machine" "vmlu" {
+  count                 = 2
+  name                  = "VMLU0${count.index + 1}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic_vmlu01.id]
+  network_interface_ids = [element(azurerm_network_interface.nic_vmlu.*.id, count.index)]
   size                  = "Standard_B1s"
 
   os_disk {
@@ -40,13 +43,13 @@ resource "azurerm_linux_virtual_machine" "vmlu01" {
     version   = "latest"
   }
 
-  computer_name                   = "vmlu01"
+  computer_name                   = "vmlu0${count.index + 1}"
   admin_username                  = "sysadmin"
   custom_data                     = filebase64("cloudinit/add_publickey.yaml")
   disable_password_authentication = true
 
   admin_ssh_key {
     username   = "sysadmin"
-    public_key = tls_private_key.ssh_vmlu01.public_key_openssh
+    public_key = element(tls_private_key.ssh_vmlu.*.public_key_openssh, count.index)
   }
 }

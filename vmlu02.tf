e4fbffcb19c4b3f48aabaf01cd63d1763ef395cd
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-05-29 13:41:22 +0300

    fix: change ubuntu image version

--- vmlu02.tf
+++ vmlu02.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_virtual_machine" "vmlu02" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
--- vmlue01.tf
+++ vmlue01.tf
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "vmlue01" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
